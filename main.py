4487488b8f68f080c236a077c0edeb1eda847e00
Luchainz <djluchainz@gmail.com>
2023-07-07 10:56:15 -0400

    Added app description image and links

--- main.py
+++ main.py
@@ -22,14 +22,23 @@ Below you can find some of the appsI have built in Python. Feel free to contact 
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5]) #dimensions are inside of brackets
 
 df = pandas.read_csv("data.csv", sep=';')
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})") #["text"](link)
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})") #["text"](link)
+
+
+
